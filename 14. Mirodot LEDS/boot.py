8fd476f8dbca94477e21ba06a3c508c8d0ea4287
Manuel Silenzi <manuelsilenzi1@gmail.com>
2023-10-10 08:32:44 -0300

    remove led blinking in boot.py

--- 14. Mirodot LEDS/boot.py
+++ 14. Mirodot LEDS/boot.py
@@ -1,14 +1,8 @@
-from machine import Pin
 from wifi_manager import WifiManager
-from time import sleep
 
-led = Pin(2, Pin.OUT, value=0)
-
-for _ in range(6):
-    led.value(not led.value())
-    sleep(0.1)
+print("\n\nStarting boot.py")
 
 wm = WifiManager()
 wm.connect()
 
-led.value(1)
+print("End boot.py")
