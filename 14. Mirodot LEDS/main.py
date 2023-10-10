17a8a405cb322eb8c51cb91f877e8df003b7fddb
Manuel Silenzi <manuelsilenzi1@gmail.com>
2023-10-10 02:00:30 -0300

    14. add API

--- 14. Mirodot LEDS/main.py
+++ 14. Mirodot LEDS/main.py
@@ -1,15 +1,45 @@
+from machine import Pin
 from microdot_asyncio import Microdot, Response, send_file
-from counter import Counter
+
 
 # Server
 app = Microdot()
 Response.default_content_type = "text/html"
 
+# Leds
+leds = tuple(Pin(p, Pin.OUT, value=0) for p in [4, 16, 2])
+# leds = (Pin(4, Pin.OUT, value=0), Pin(16, Pin.OUT, value=0), Pin(2, Pin.OUT, value=0))
+
+
+def get_leds_values():
+    """returns a list of the LEDs values"""
+    return list(map(lambda led: led.value(), leds))
+
 
 # API
 
 
-# Api go brrr...
+@app.get("/leds")
+async def get_all_leds_values(request):
+    return get_leds_values()
+
+
+@app.post("/leds/<int:id>/off")
+async def turn_off_led(request, id):
+    if 0 <= id < len(leds):
+        leds[id].off()
+        return Response(status_code=204)
+    else:
+        return {"error": "Invalid LED ID"}, 404
+
+
+@app.post("/leds/<int:id>/on")
+async def turn_on_led(request, id):
+    if 0 <= id < len(leds):
+        leds[id].on()
+        return Response(status_code=204)
+    else:
+        return {"error": "Invalid LED ID"}, 404
 
 
 # Static files
@@ -28,4 +58,4 @@ async def static(request, path):
     return send_file("static/" + path)
 
 
-app.run(debug=True)
+app.run()
