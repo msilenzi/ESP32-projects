b0d7e57b3bb73496098446e2790144d95bf672b2
Manuel Silenzi <manuelsilenzi1@gmail.com>
2023-10-13 17:57:54 -0300

    create 15. Microdot RGB

---
+++ 15. Microdot RGB/boot.py
@@ -0,0 +1,8 @@
+from wifi_manager import WifiManager
+
+print("\n\nStarting boot.py")
+
+wm = WifiManager()
+wm.connect()
+
+print("End boot.py")
---
+++ 15. Microdot RGB/main.py
@@ -0,0 +1,30 @@
+from microdot_asyncio import Microdot, Response, send_file
+
+# Server
+app = Microdot()
+Response.default_content_type = "text/html"
+
+
+# API
+
+
+# API go brrr
+
+
+# Static files
+
+
+@app.route("/")
+async def index(request):
+    return send_file("static/index.html")
+
+
+@app.route("/static/<path:path>")
+async def static(request, path):
+    if ".." in path:
+        # directory traversal is not allowed
+        return "Not found", 404
+    return send_file("static/" + path)
+
+
+app.run()
